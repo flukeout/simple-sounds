d354619a3a379330246fe6aaeb60b107f4c241b1
Luke Pacholski <luke.pacholski@gmail.com>
2016-05-05 10:15:21 -0700

    Updated so that if the file can't be be loaded with AJAX we use an audio element.

--- sounds.js
+++ sounds.js
@@ -32,27 +32,48 @@ for(var key in sounds) {
 }
 
 function loadSound(name){
-  var url = sounds[name].url;
-  var buffer = sounds[name].buffer;
+  var sound = sounds[name];
+
+  var url = sound.url;
+  var buffer = sound.buffer;
 
   var request = new XMLHttpRequest();
   request.open('GET', url, true);
   request.responseType = 'arraybuffer';
 
+  // First - we try to load the file with an AJAX request...
   request.onload = function() {
     soundContext.decodeAudioData(request.response, function(newBuffer) {
-      sounds[name].buffer = newBuffer;
+      sound.type = "buffer";
+      sound.buffer = newBuffer;
     });
   }
+
+  // If that fails, we instead create an Audio element
+  request.onerror = function(e){
+    sound.type = "element";
+    sound.element = document.createElement('audio');
+    sound.element.src = sound.url;
+  }
+
   request.send();
 }
 
 function playSound(name){
-  var buffer = sounds[name].buffer;
-  if(buffer){
-    var source = soundContext.createBufferSource();
-    source.buffer = buffer;
-    source.connect(soundContext.destination);
-    source.start(0);
+
+  var sound = sounds[name];
+
+  if(sound.type == "buffer") {
+    var buffer = sound.buffer;
+    if(buffer){
+      var source = soundContext.createBufferSource();
+      source.buffer = buffer;
+      source.connect(soundContext.destination);
+      source.start(0);
+    }
+  } else {
+    sound.element.pause();
+    sound.element.currentTime = 0;
+    sound.element.play();
   }
 }
